dd1c6331560272b09d0c854be0fc10c9f744258e
unknown <mourya123-mo>
2024-02-14 20:10:20 +0530

    added prototype router

--- src/Component/Util/Header.jsx
+++ src/Component/Util/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../logo.png'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../logo.png';
 
 const Header = () => {
+    const [showLoginDropdown, setShowLoginDropdown] = useState(false);
+
+    const LoginDropdown = () => {
+        setShowLoginDropdown(!showLoginDropdown);
+    };
+
     return (
         <div>
             <header className='bg-slate-400 h-20 flex items-center justify-between px-8'>
@@ -19,22 +25,34 @@ const Header = () => {
                 {/* Navigation Links */}
                 <nav className='space-x-6'>
                     {/* Login */}
-                    <Link to="/" className="text-white hover:text-blue-500">
-                        Login
-                    </Link>
+                    <div className="relative inline-block text-left">
+                        <button onClick={LoginDropdown} className="text-white hover:text-blue-500 bg-sky-700 p-2 rounded">
+                            Login
+                        </button>
+                        {showLoginDropdown && (
+                            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded ">
+                                <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Sign-up</Link>
+                                {/* <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Option 2</Link> */}
+                                {/* <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Option 3</Link> */}
+                            </div>
+                        )}
+                    </div>
                     {/* Become a Seller */}
-                    <Link to="/" className="text-white hover:text-blue-500">
+                    <Link to="/" className="text-white hover:text-blue-500 bg-sky-700 p-2 rounded">
                         Become a seller
                     </Link>
+                    {/* Signup */}
+                    {/* <Link to="/" className="text-white hover:text-blue-500 bg-sky-700 p-2 rounded">
+                        Signup
+                    </Link> */}
                     {/* Cart */}
-                    <Link to="/" className="text-white hover:text-blue-500">
+                    <Link to="/" className="text-white hover:text-blue-500 bg-sky-700 p-2 rounded">
                         Cart
                     </Link>
                 </nav>
             </header>
         </div>
-        
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navs from "./Component/Routes/Navigation.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  
-  </React.StrictMode>,
-)
+const user = {
+    username: "",
+    role: "CUSTOMER",
+    isAuthenticated: false
+}
+
+const { role, isAuthenticated } = user;
+const allRoutes = () => {
+    return (
+        <Route path={"/"} element={<App />}>
+            {Navs.map((nav, i) => {
+                if (isAuthenticated) {
+                    if (nav.isVisibleAfterAuth) {
+                        if (nav.role === role || nav.role === "ALL") {
+                            console.log(nav);
+                            return <Route key={i} path={nav.path} element={nav.element} />
+                        }
+
+                    }
+                } else {
+                    if (!nav.requireAuth && nav.role === "ALL") {
+                        console.log(nav);
+                        return <Route key={i} path={nav.path} element={nav.element} />
+                    }
+                }
+            })}
+        </Route>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            {/* <App/> */}
+            <Routes>
+                {allRoutes()}
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
